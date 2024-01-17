30a21710f9edb6c34f6772f05383699ebf3b4e91
curiousemmanuel <curiouswrld1@gmail.com>
2024-01-17 23:17:51 +0300

    first 3 lessons to introduce php

---
+++ 1.embeded.php
@@ -0,0 +1,8 @@
+<?php 
+// this may not be the correct order to learn but its a guarantee for your mastery in php
+echo "welcome to php";
+/*after every php function you must end with a semi collon 
+ this tells apache that you are done with that line and you are ready to start a new one */
+
+
+?>
---
+++ 2.dynamic.php
@@ -0,0 +1,36 @@
+<?php 
+
+//using dynamic data
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+ 
+ <?php 
+
+$title = "PHP website";// since this title is already written here you do not need to repeat it at every single page that you want it to apear
+    
+?>
+  
+  
+
+  
+ <h1><?php echo $title; ?></h1> <?php /*you just need to echo it out and its done
+dynamic data or images or files are very useful especialy whe it is needed in multiple pages 
+The coolest thing about dynamic data is when changes are needed you only need to edid or change one file in one page and others will be changed automatically
+N/B it is mainly written in function.php file
+*/
+  ?>
+   
+   
+   
+    
+</body>
+</html>
+
+
+
